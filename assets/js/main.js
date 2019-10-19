7726c939d9e609c5c8bd4a983dcb3eb2f3faae87
shantoztech <45969365+shantoztech@users.noreply.github.com>
2019-10-20 04:57:55 +0600

    pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
 (function($) {
 
+// fixed-header
+
+window.onscroll = function(){
+  scrollFunction()
+};
+function scrollFunction(){
+  var windowWidth = $(window).width();  
+  if(document.body.scrollTop>600||document.documentElement.scrollTop>600){
+    $(".hh-fxd-hdr").css({'top':'0','z-index':'99'});
+
+  }else{
+    $(".hh-fxd-hdr").css('top', '-110px');
+  }
+}
+
+
 })(jQuery);
-
-
-
-
 
 
 
